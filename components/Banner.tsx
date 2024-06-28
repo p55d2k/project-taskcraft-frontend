3bbd18d55c9ef0d2f1450324ce8e70f5cecc0d71
p55d2k <231637w@student.hci.edu.sg>
2024-06-28 16:22:32 +0800

    feat: homepage improvement

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,29 +5,27 @@ const kanit = Kanit({ subsets: ["latin"], weight: ["600"] });
 
 const Banner = () => {
   return (
-    <div className="flex flex-col">
-      <div className="h-screen flex flex-col lg:flex-row justify-between">
-        <div className="w-full h-full flex flex-col space-y-3 md:space-y-5 items-center lg:items-start px-16 lg:px-28 justify-center">
-          <h1
-            className={`${kanit.className} text-center lg:text-left text-4xl md:text-5xl lg:text-6xl  uppercase`}
-          >
-            TASKCRAFT: THE AI-INTEGRATED TOOL FOR PROJECT MANAGEMENT
-          </h1>
-          <p className="text-xl md:text-2xl lg:text-3xl">
-            TaskCraft is the simplest and easiest way to manage your projects
-            with AI integration. Get started today!
-          </p>
-        </div>
-        <div className="w-full h-full flex items-end justify-center">
-          <Image
-            src="imgs/catgirl.png"
-            alt=""
-            width={100}
-            height={100}
-            className="w-full"
-            unoptimized
-          />
-        </div>
+    <div className="min-h-screen pt-24 lg:pt-0 flex flex-col lg:flex-row -space-y-24 sm:space-y-0 justify-between">
+      <div className="w-full h-full lg:min-h-screen flex flex-col space-y-3 md:space-y-5 items-center lg:items-start px-16 lg:px-28 lg:pt-10 justify-center">
+        <h1
+          className={`${kanit.className} text-center lg:text-left text-4xl md:text-5xl lg:text-6xl  uppercase`}
+        >
+          TASKCRAFT: THE AI-INTEGRATED TOOL FOR PROJECT MANAGEMENT
+        </h1>
+        <p className="text-xl md:text-2xl xl:text-4xl text-center lg:text-left">
+          TaskCraft is the simplest and easiest way to manage your projects with
+          AI integration. Get started today!
+        </p>
+      </div>
+      <div className="w-full min-h-screen lg:h-auto -z-10 flex items-end justify-center">
+        <Image
+          src="imgs/catgirl.png"
+          alt=""
+          width={100}
+          height={100}
+          className="w-full opacity-70"
+          unoptimized
+        />
       </div>
     </div>
   );
